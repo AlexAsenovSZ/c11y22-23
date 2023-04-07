d9916f1ae8084f33e04943844b62d664db041bef
Aleksandar Asenov <aleksandar.asenov@zaimov-pl.com>
2023-04-06 23:37:04 -0700

    Ivan Zadacha work text

---
+++ Tutorial/namiraneNaSimvoli/namiraneNaSimvoli/Ivan.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namiraneNaSimvoli
+{
+    class Ivan
+    {
+        public int lettersCount(string text)
+        {
+            int charCounter = 0;
+
+            foreach (char letter in text)
+            {
+
+                if (letter != ' ')
+                {
+                    charCounter++;
+                }
+            }
+
+            return charCounter;
+        }
+
+        public int wordsCounter(string text)
+        {
+            int wordCounter = 0;
+
+            if (text != "" && text != " ")
+            {
+                wordCounter = 1;
+                foreach (char item in text)
+                {
+                    if (item ==' ')
+                    {
+                        wordCounter++;
+                    }
+                }
+            }
+
+            return wordCounter;
+        }
+
+        public int points(string text, int expectedWordsCount, int expectedCharCount)
+        {
+            int charCounter = 0;
+            int wordCounter = 0;
+            int pointsCounter = 0;
+
+            foreach (char letter in text)
+            {
+
+                if (letter != ' ')
+                {
+                    charCounter++;
+                }
+            }
+
+            if (text != "" && text != " ")
+            {
+                wordCounter = 1;
+                foreach (char item in text)
+                {
+                    if (item == ' ')
+                    {
+                        wordCounter++;
+                    }
+                }
+            }
+
+            if ((wordCounter < expectedWordsCount * 0.5) || (charCounter < expectedCharCount * 0.5))
+            {
+                pointsCounter += 0;
+            }
+            else if ((wordCounter >= expectedWordsCount * 0.5 && wordCounter <= expectedWordsCount * 0.75) || charCounter >= expectedCharCount * 0.5 && charCounter <= expectedCharCount * 0.75)
+            {
+                pointsCounter++;
+            }
+            else
+            {
+                pointsCounter += 2;
+            }
+
+            return pointsCounter;
+        }
+    }
+}
---
+++ Tutorial/namiraneNaSimvoli/namiraneNaSimvoli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namiraneNaSimvoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ivan s = new Ivan();
+            Console.Write("Letters count: ");
+            Console.WriteLine(s.lettersCount("Hello World"));
+            Console.Write("Words count: ");
+            Console.WriteLine(s.wordsCounter("Hello World"));
+            Console.Write("Points: ");
+            Console.WriteLine(s.points("Hello World", 2, 10));
+        }
+    }
+}
