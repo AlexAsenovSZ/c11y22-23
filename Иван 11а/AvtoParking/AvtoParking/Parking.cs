22454b188c3b61dc6cd4cbc77a50f8d292216e55
Aleksandar Asenov <aleksandar.asenov@zaimov-pl.com>
2023-04-27 23:38:28 -0700

    Ivan Begin AvtoParking

---
+++ Иван 11а/AvtoParking/AvtoParking/Parking.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoParking
+{
+    internal class Parking
+    {
+        protected int[] position = new int[20];
+        protected int freeTime = 1; //1h
+        protected int priceTime = 2;
+        int counter;
+        int[] queue = new int[1];
+        int queueCounter;
+        public void Enter()
+        {
+            for (int i = 0; i < position.Length; i++)
+            {
+                position[i] = int.Parse(Console.ReadLine());
+                if (position[i] == 0)
+                {
+                    Console.WriteLine("Welcome!");
+                    break;
+                }
+                if (counter > 20)
+                {
+                    Console.WriteLine("The parking is full!");
+                    Console.WriteLine("Please wait in the queue!");
+                    queueCounter++;
+                    queue = new int[queueCounter];
+                    break;
+                }
+                else
+                {
+                    counter += position[i];
+                }
+            }
+
+            
+        }
+        public void Exit()
+        {
+            counter--;
+
+            if (counter < 20)
+            {
+                
+            }
+        }
+
+
+    }
+}
---
+++ Иван 11а/AvtoParking/AvtoParking/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parking Pmol = new Parking();
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Welcome!");
+                Console.Write("enter (t) or exit (f): ");
+                input = Console.ReadLine();
+                if (input == "t")
+                {
+                    Pmol.Enter();
+                }
+                else
+                {
+                    Pmol.Exit();
+                }
+                Console.WriteLine("Working is parking (t/f)");
+                input = Console.ReadLine();
+                if (input == "f")
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
